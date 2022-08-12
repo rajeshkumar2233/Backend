0f17920debe34c126004bda556e512268bc251d6
rajeshmajhi2233 <rajeshmajhi2233@gmail.com>
2022-08-12 12:53:13 +0530

    Post API Assignment 2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,56 +1,44 @@
+
 const express = require('express');
-const router = express.Router();
+const app = express.Router();
 
 
 
-let players =
-    [
-        {
-            "name": "manish",
-            "dob": "1/1/1995",
-            "gender": "male",
-            "city": "jalandhar",
-            "sports": [
-                "swimming"
-            ]
-        },
-        {
-            "name": "gopal",
-            "dob": "1/09/1995",
-            "gender": "male",
-            "city": "delhi",
-            "sports": [
-                "soccer"
-            ]
-        },
-        {
-            "name": "lokesh",
-            "dob": "1/1/1990",
-            "gender": "male",
-            "city": "mumbai",
-            "sports": [
-                "soccer"
-            ]
-        },
-    ]
 
-router.post('/players', function (req, res) {
-    let status = true;
-    for (let i = 0; i < players.length; i++) {
-        if (players[i]["name"] === req.body.name) {
-            console.log("This player was already added")
-            status = false
-            break
-        }
+let persons = [
+    {
+      name : "PK",
+      age : 10,
+      votingStatus : false
+    },
+    {
+      name : "SKk",
+      age : 20,
+      votingStatus : false
+    },
+    {
+      name : "AA",
+      age : 70,
+      votingStatus : false
+    },
+    {
+      name : "SC",
+      age : 5,
+      votingStatus : false
+    },
+    {
+      name : "HO",
+      age : 40,
+      votingStatus : false
     }
-    if (status === true) {
-        console.log("Record Inserted ")
-        players.push(req.body)
-    }
-
-    console.log(players)
-
-    res.send("done")
-});
-
-module.exports = router;
+  ]
+  
+  app.post('/votingStatus', function(req,res){
+    let voteAge = req.query.age;
+    let votePersons = persons.filter(index => index.age >= voteAge)
+   votePersons.forEach(index => index.votingStatus = true);
+  
+    res.send(votePersons);
+  })
+  
+  module.exports = app
