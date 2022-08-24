aee4cc3a62e18785195ae252f592ca15be946206
rajeshmajhi2233 <rajeshmajhi2233@gmail.com>
2022-08-24 19:28:36 +0530

    assignment/middleware-1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const moment= require('moment')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,12 +15,18 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
+
+
 app.use (
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        const date=moment().format('DD-MM-YYYY, HH:mm:ss');
+        const ipAddress= req.ip;
+        const r=req.originalUrl
+        console.log (date," , ",ipAddress," , ",r);
         next();
   }
   );
+
 
 app.use('/', route);
 
