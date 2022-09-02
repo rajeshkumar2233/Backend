c753feaff67ba7298f9f486a2bc10d05b1f9d447
rajeshmajhi2233 <rajeshmajhi2233@gmail.com>
2022-09-02 12:30:52 +0530

    assignments/axios-cowin

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const weatherController= require("../controllers/weatherController")
+const CowinController= require("../controllers/cowinController")
 
 
-//--------------------------------------------------------//
 
 router.get("/test-me", function (req, res) {
-    res.send("My server is running awesome!")
+    res.send("My first ever api!")
 })
-//--------------------------------------------------------//
 
-router.get("/weather/city",weatherController.getweather)
 
-router.get("/weather/my-cities", weatherController.sortByCities)
+router.get("/cowin/states", CowinController.getStates)
+router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
+router.get("/cowin/getByPin", CowinController.getByPin)
 
-router.get("/weather/city/minmax",weatherController.sortByMin)
-module.exports = router; 
+router.post("/cowin/getOtp", CowinController.getOtp)
+
+router.get("/cowin/session/district",CowinController.getByDistrictId)
+
+
+module.exports = router;
